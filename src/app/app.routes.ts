b96f7166c7c57e7033af5f54b16499c968a6a53d
Tamara García <tamaragarcia.dev@gmail.com>
2025-03-10 20:45:02 +0100

    componentes creados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent, title: 'Home Page' },
+  { path: 'about', component: AboutComponent, title: 'About Us' },
+  { path: 'contact', component: ContactComponent, title: 'Contact Us' },
+  { path: 'projects', component: ProjectComponent, title: 'Our Projects' },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `home`
+  { path: '**', component: HomeComponent },
+];
